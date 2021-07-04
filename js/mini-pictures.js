c347de815d6e034dd23333f075745964d6260ce8
Svetlana-Saloa <78179270+Svetlana-Saloa@users.noreply.github.com>
2021-07-04 11:34:48 +0300

    Отрисуй меня полностью (часть 1) правки

--- js/mini-pictures.js
+++ js/mini-pictures.js
@@ -1,21 +1,21 @@
 import {createPhotos} from './data.js';
 
-const loadingMiniPictures = document.querySelector('.pictures');
+const miniPicturesContainer = document.querySelector('.pictures');
 
 const templatePicture = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const creatMiniPictures = createPhotos();
+const miniPictures = createPhotos();
 
-const creatMiniPhotoFragment = document.createDocumentFragment();
+const miniPictureFragment = document.createDocumentFragment();
 
-creatMiniPictures.forEach(({url, likes, comments}) => {
+miniPictures.forEach(({url, likes, comments}) => {
   const miniPictureElement = templatePicture.cloneNode(true);
   miniPictureElement.querySelector('.picture__img').src = url;
   miniPictureElement.querySelector('.picture__likes').innerHTML= likes;
   miniPictureElement.querySelector('.picture__comments').textContent = comments.length;
-  creatMiniPhotoFragment.appendChild(miniPictureElement);
+  miniPictureFragment.appendChild(miniPictureElement);
 });
 
-loadingMiniPictures.appendChild(creatMiniPhotoFragment);
+miniPicturesContainer.appendChild(miniPictureFragment);
